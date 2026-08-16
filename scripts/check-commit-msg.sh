2f43205954e56e836bdbd0787a9d3e955dd84da5
scttbnsn <80784472+scttbnsn@users.noreply.github.com>
2026-08-16 19:40:32 -0400

    fix(hooks): exempt only git-generated merge and revert subjects

--- scripts/check-commit-msg.sh
+++ scripts/check-commit-msg.sh
@@ -6,8 +6,12 @@ msg_file="$1"
 subject="$(head -n 1 "$msg_file")"
 
 case "$subject" in
-  "Merge branch "*|"Merge pull request "*|"Merge remote-tracking branch "*|"Revert \""*|fixup!\ *|squash!\ *) exit 0 ;;
+  "Merge branch '"*|"Merge pull request #"*|"Merge remote-tracking branch '"*|fixup!\ *|squash!\ *) exit 0 ;;
 esac
+
+if [[ "$subject" =~ ^Revert\ \".+\"$ ]]; then
+  exit 0
+fi
 
 pattern='^(feat|fix|docs|style|refactor|perf|test|build|ci|chore|revert)(\([a-z0-9./-]+\))?!?: [^[:space:]]'
 if ! [[ "$subject" =~ $pattern ]]; then
